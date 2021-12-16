d75a3976a461b9f1a91870a3af0b599b4210037e
Claudia Silva <csilva@allin.com.br>
2021-12-16 10:23:42 -0300

    fix: alter propsFood styles

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,10 +1,10 @@
 import styled, { css } from 'styled-components';
 
-type FoodProps = {
+type PropsFood = {
   available: boolean;
 }
 
-export const Container = styled.div<FoodProps>`
+export const Container = styled.div<PropsFood>`
   background: #f0f0f5;
   border-radius: 8px;
 
